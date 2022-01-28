f8e07c073ef6bd785612b6b51abe09f7f7112f55
krishna-thapa <krishna.nature91@gmail.com>
2022-01-28 15:56:12 +0000

    Added custom exception handling and dotenv

--- quotesapi/src/lib.rs
+++ quotesapi/src/lib.rs
@@ -1,5 +1,14 @@
-use std::error::Error; 
 use serde::Deserialize;
+
+#[derive(thiserror::Error, Debug)]
+pub enum QuotesApiError{
+    #[error("Failed fetching quotes")]
+    RequestFailed(ureq::Error),
+    #[error("Failed convedrting response to string")]
+    FailedResponseToString(std::io::Error),
+    #[error("Quote Parsing Failed")]
+    QuoteParseFailed(serde_json::Error)
+}
 
 #[derive(Deserialize, Debug)]
 #[allow(non_snake_case)]
@@ -11,13 +20,13 @@ pub struct Quote {
     pub genre: String
 }
 
-pub fn get_quotes(url: &str) -> Result<Vec<Quote>, Box<dyn Error>> {
+pub fn get_quotes(url: &str) -> Result<Vec<Quote>, QuotesApiError> {
     let response: String = ureq::get(url)
         .set("accept", "application/json")
-        .call()?
-        .into_string()?;
+        .call().map_err(|e| QuotesApiError::RequestFailed(e))?
+        .into_string().map_err(|e| QuotesApiError::FailedResponseToString(e))?;
     
-    let quotes: Vec<Quote> = serde_json::from_str(&response)?;
+    let quotes: Vec<Quote> = serde_json::from_str(&response).map_err(|e| QuotesApiError::QuoteParseFailed(e))?;
 
     Ok(quotes)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 
 use colour::{yellow, magenta, green, cyan, red};
 
+use dotenv::dotenv;
+
 use quotesapi::{get_quotes, Quote};
 
 fn render_quotes(quotes: &Vec<Quote>) {
@@ -16,6 +18,12 @@ fn render_quotes(quotes: &Vec<Quote>) {
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("=== Running the main function! ===");
+    
+    dotenv()?;
+    let api_key: String = std::env::var("API_KEY")?;
+
+    println!("=== API KEY to be used: {} ===", api_key);
+
     let url: &str = "http://localhost:9000/quote/randomTen";
     let quotes: Vec<Quote> = get_quotes(url)?;
     
